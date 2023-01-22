c006a5e213c256a25fee1a9f585b690c275bff83
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-01-23 00:44:28 +0530

    feat: add suite and tests

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("Test Greeting Component", () => {
-  //AAA
-  //Arrange
-  render(<Greeting />);
-  //Act
-  // nothing
-
-  //Assert
-  const visitor = screen.getByText("Hey You!", { exact: false });
-  expect(visitor).toBeInTheDocument();
+describe('<Greeting/> component tests', ()=>{
+    test("render hey you as a text", () => {
+        //AAA
+        //Arrange
+        render(<Greeting />);
+        //Act
+        // nothing
+      
+        //Assert
+        const visitor = screen.getByText("Hey You!", { exact: false });
+        expect(visitor).toBeInTheDocument();
+      });
 });
