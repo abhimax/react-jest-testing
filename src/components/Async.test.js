48a87344e4039cd2e6ce645c3bef61a9f7c82413
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-02-01 03:17:15 +0530

    feat:  fetch Mock test

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -6,5 +6,14 @@ describe('Async Component', ()=>{
         render (<Async/>);
         const listItemElements = await screen.findAllByRole('listitem'); // if getAllByRole fails for async tasks
         expect(listItemElements).not.toHaveLength(0);
+    });
+        test('renders the list if the request succeeds [Mocks]', async () => {
+            window.fetch = jest.fn();
+            window.fetch.mockResolvedValueOnce({
+                json: async () => [{ id: 'p1', title:'First post'}],
+            });
+        render (<Async/>);
+        const listItemElements = await screen.findAllByRole('listitem'); // if getAllByRole fails for async tasks
+        expect(listItemElements).not.toHaveLength(0);
     })
 });
